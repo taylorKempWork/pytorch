9adf7c01ce49e0c89e1ba5d6e9560847bbe8510a
Catherine Lee <csl@fb.com>
2022-10-12 14:56:56 -0700

    change ordering

--- test/functorch/conftest.py
+++ test/functorch/conftest.py
@@ -147,20 +147,21 @@ class LogXMLReruns(LogXML):
 
 
 # imitating summary_failures in pytest's terminal.py
+# both hookwrapper and tryfirst to make sure this runs before pytest's
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
     # prints stack traces for reruns
     if terminalreporter.config.option.tbstyle != "no":
         reports = terminalreporter.getreports("rerun")
-        if not reports:
-            return
-        terminalreporter.write_sep("=", "RERUNS")
-        if terminalreporter.config.option.tbstyle == "line":
-            for rep in reports:
-                line = terminalreporter._getcrashline(rep)
-                terminalreporter.write_line(line)
-        else:
-            for rep in reports:
-                msg = terminalreporter._getfailureheadline(rep)
-                terminalreporter.write_sep("_", msg, red=True, bold=True)
-                terminalreporter._outrep_summary(rep)
-                terminalreporter._handle_teardown_sections(rep.nodeid)
+        if reports:
+            terminalreporter.write_sep("=", "RERUNS")
+            if terminalreporter.config.option.tbstyle == "line":
+                for rep in reports:
+                    line = terminalreporter._getcrashline(rep)
+                    terminalreporter.write_line(line)
+            else:
+                for rep in reports:
+                    msg = terminalreporter._getfailureheadline(rep)
+                    terminalreporter.write_sep("_", msg, red=True, bold=True)
+                    terminalreporter._outrep_summary(rep)
+                    terminalreporter._handle_teardown_sections(rep.nodeid)
