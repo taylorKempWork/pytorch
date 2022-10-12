f25b64521238659097499fe0ddcd74f8a2e42c79
Peter Bell <peterbell10@live.co.uk>
2022-10-12 23:25:45 +0100

    OpInfo: Add test that sample_inputs_func returns a generator
    This also includes a small list exception for single element lists.
    
    ghstack-source-id: 6df83a3e26420111e684efd6178b09e7e762446d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/84567

--- torch/testing/_internal/common_methods_invocations.py
+++ torch/testing/_internal/common_methods_invocations.py
@@ -9869,6 +9869,12 @@ op_db: List[OpInfo] = [
                    }),
                    "TestDecomp", "test_comprehensive", device_type="cuda",
                ),
+               DecorateInfo(
+                   toleranceOverride({
+                       torch.complex64: tol(atol=5e-5, rtol=5e-5),
+                   }),
+                   "TestDecomp", "test_comprehensive", device_type="cuda",
+               ),
            ],
            skips=(
                # Strides are not the same!
